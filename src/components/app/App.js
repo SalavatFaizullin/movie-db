626aebd6b0c8abd32c4184e0954a5f24465a4157
SalavatFaizullin <windou7@gmail.com>
2023-03-07 14:35:54 +0500

    добавил поиск

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,41 +1,57 @@
 /* eslint-disable */
 import React, { Component } from 'react'
 
+import './App.css'
+
 import MovieList from '../movie-list'
 import MoviedbService from '../../services/moviedbService'
 import { Online, Offline } from 'react-detect-offline'
 import ErrorAlert from '../error-alert/ErrorAlert'
 import OfflineAlert from '../offline-alert/OfflineAlert'
 import Spinner from '../spinner/Spinner'
+import SearchArea from '../search-area/SearchArea'
+import { debounce } from 'lodash'
 
 class App extends Component {
   MoviedbService = new MoviedbService()
 
-  constructor() {
-    super()
-    this.state = {
-      movies: [],
+  state = {
+    movies: [],
+    loading: false,
+    searchTerm: '',
+    error: false,
+    errorMessage: '',
+  }
+
+  debounceSearch = debounce((e) => {
+    this.setState({
+      searchTerm: e.target.value,
       loading: true,
-      error: false,
-    }
-    this.getMovies()
-  }
-
-  getMovies = () => {
-    this.MoviedbService.getMovies('return')
-      .then((data) => {
-        this.setState({
-          movies: data.results,
-          loading: false,
+    })
+    if (this.state.searchTerm !== '') {
+      this.MoviedbService.getMovies(this.state.searchTerm)
+        .then((data) => {
+          this.setState({
+            movies: data.results,
+            loading: false,
+            searchTerm: '',
+          })
         })
-      })
-      .catch(this.onError)
+        .catch(this.onError)
+    } else {
+      this.debounceSearch(e)
+    }
+  }, 1000)
+
+  handleKeyUp = (e) => {
+    this.debounceSearch(e)
   }
 
-  onError = (err) => {
+  onError = (error) => {
     this.setState({
       error: true,
       loading: false,
+      errorMessage: error.mesage,
     })
   }
 
@@ -44,18 +60,28 @@ class App extends Component {
     const hasData = !(loading || error)
     const errorAlert = error ? <ErrorAlert /> : null
     const offlineAlert = <OfflineAlert />
-    const spinner = loading ? <Spinner /> : null
-    const content = hasData ? <MovieList movies={movies} /> : null
+    const spinner = loading ? (
+      <>
+        {/* <SearchArea handleSubmit={this.handleSubmit} handleKeyUp={this.handleKeyUp} /> */}
+        <Spinner />
+      </>
+    ) : null
+    const content = hasData ? (
+      <>
+        <SearchArea handleSubmit={this.handleSubmit} handleKeyUp={this.handleKeyUp} />
+        <MovieList movies={movies} />
+      </>
+    ) : null
 
     return (
-      <>
+      <div className='app'>
         <Online>
           {errorAlert}
           {spinner}
           {content}
         </Online>
         <Offline>{offlineAlert}</Offline>
-      </>
+      </div>
     )
   }
 }
